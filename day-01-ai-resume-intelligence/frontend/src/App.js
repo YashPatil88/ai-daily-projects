43af43b40fa9cede1df08eb9ba99a8fbd4487b0a
Yash Patil <yashspatil4779@gmail.com>
2026-01-31 12:42:04 +0530

    Auto commit: AI Smart Resume Intelligence Platform

---
+++ day-01-ai-resume-intelligence/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AI Resume Intelligence Platform</h1>
+      <p>Upload resume and get AI-powered insights.</p>
+    </div>
+  );
+}
+
+export default App;
